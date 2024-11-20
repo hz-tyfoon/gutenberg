cababf770d8b14c073041bacbc8889a241774e1a
Matias Benedetto <matias.benedetto@gmail.com>
2024-11-20 11:00:58 -0300

    add icon background color

--- packages/block-editor/src/components/block-toolbar/switch-section-style.js
+++ packages/block-editor/src/components/block-toolbar/switch-section-style.js
@@ -4,55 +4,110 @@
 import {
 	ToolbarButton,
 	ToolbarGroup,
-	Dropdown,
-	__experimentalDropdownContentWrapper as DropdownContentWrapper,
 	Icon,
+	Path,
+	SVG,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { color } from '@wordpress/icons';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useContext } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import BlockStyles from '../block-styles';
 import useStylesForBlocks from '../block-styles/use-styles-for-block';
+import { replaceActiveStyle } from '../block-styles/utils';
+import { store as blockEditorStore } from '../../store';
+import { GlobalStylesContext } from '../global-styles';
+import { globalStylesDataKey } from '../../store/private-keys';
+import { getVariationStylesWithRefValues } from '../../hooks/block-style-variation';
 
-const POPOVER_PROPS = {
-	placement: 'bottom-start',
-};
+const styleIcon = (
+	<SVG
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<Path
+			stroke="currentColor"
+			strokeWidth="1"
+			d="M17.2 10.9c-.5-1-1.2-2.1-2.1-3.2-.6-.9-1.3-1.7-2.1-2.6L12 4l-1 1.1c-.6.9-1.3 1.7-2 2.6-.8 1.2-1.5 2.3-2 3.2-.6 1.2-1 2.2-1 3 0 3.4 2.7 6.1 6.1 6.1s6.1-2.7 6.1-6.1c0-.8-.3-1.8-1-3z"
+		/>
+	</SVG>
+);
 
 function SwitchSectionStyle( { clientId } ) {
-	const { stylesToRender } = useStylesForBlocks( { clientId } );
+	const { stylesToRender, activeStyle, className } = useStylesForBlocks( {
+		clientId,
+	} );
+	const { updateBlockAttributes } = useDispatch( blockEditorStore );
+
+	// Get global styles data
+	const { merged: mergedConfig } = useContext( GlobalStylesContext );
+	const { globalSettings, globalStyles, blockName } = useSelect(
+		( select ) => {
+			const settings = select( blockEditorStore ).getSettings();
+			return {
+				globalSettings: settings.__experimentalFeatures,
+				globalStyles: settings[ globalStylesDataKey ],
+				blockName: select( blockEditorStore ).getBlockName( clientId ),
+			};
+		},
+		[ clientId ]
+	);
+
+	// Get the background color for the active style
+	const activeStyleBackground = activeStyle?.name
+		? getVariationStylesWithRefValues(
+				{
+					settings: mergedConfig?.settings ?? globalSettings,
+					styles: mergedConfig?.styles ?? globalStyles,
+				},
+				blockName,
+				activeStyle.name
+		  )?.color?.background
+		: undefined;
 
 	if ( ! stylesToRender || stylesToRender.length === 0 ) {
 		return null;
 	}
 
+	const handleStyleSwitch = () => {
+		const currentIndex = stylesToRender.findIndex(
+			( style ) => style.name === activeStyle.name
+		);
+
+		const nextIndex = ( currentIndex + 1 ) % stylesToRender.length;
+		const nextStyle = stylesToRender[ nextIndex ];
+
+		const styleClassName = replaceActiveStyle(
+			className,
+			activeStyle,
+			nextStyle
+		);
+
+		updateBlockAttributes( clientId, {
+			className: styleClassName,
+		} );
+	};
+
 	return (
-		<Dropdown
-			popoverProps={ POPOVER_PROPS }
-			renderToggle={ ( { onToggle, isOpen } ) => {
-				return (
-					<ToolbarGroup>
-						<ToolbarButton
-							onClick={ () => onToggle( ! isOpen ) }
-							aria-expanded={ isOpen }
-							label={ __( 'Styles' ) }
-						>
-							<Icon icon={ color } />
-						</ToolbarButton>
-					</ToolbarGroup>
-				);
-			} }
-			renderContent={ () => (
-				<DropdownContentWrapper
-					className="block-editor-block-toolbar-change-design-content-wrapper"
-					paddingSize="none"
-				>
-					<BlockStyles clientId={ clientId } />
-				</DropdownContentWrapper>
-			) }
-		/>
+		<ToolbarGroup>
+			<ToolbarButton
+				onClick={ handleStyleSwitch }
+				label={ __( 'Switch style' ) }
+			>
+				<Icon
+					icon={ styleIcon }
+					style={ {
+						fill: activeStyleBackground || 'transparent',
+					} }
+				/>
+			</ToolbarButton>
+		</ToolbarGroup>
 	);
 }
 
